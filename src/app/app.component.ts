1f393f81c2e2e894431e99da00c0ff28b6ef1ae8
Lindalberto <lindalbertorvcf@gmail.com>
2018-01-26 21:30:51 -0200

    feat: Arquivos que estavam faltando.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './auth/auth.service';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
 }
---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'vWsmhHgefltQ1HVG6Oh3ixKeyx2k013b',
+  domain: 'gco-dev.auth0.com',
+  callbackURL: 'https://gco-dev.auth0.com/mobile'
+};
